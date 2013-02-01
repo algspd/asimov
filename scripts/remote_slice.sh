09e74a19736fb1d2ba6f30ca0940bf6cfa21a00f
Alejandros S <alejandroscf@gmail.com>
2013-02-01 20:34:48 +0100

    remote_slice paramter printer

--- scripts/remote_slice.sh
+++ scripts/remote_slice.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 trap "exit 1" SIGINT SIGTERM
+
 filename=~/stl/$(basename $1)
 outname=$2
-remotehost=p_giskard@pulsar.unizar.es
+#remotehost=p_ester@pulsar.unizar.es
+remotehost=$3
 # Script para lanzar sliceado en remoto
 #echo "Borrando .skeinforge remoto"
 #ssh $remotehost 'rm -rf .skeinforge'
@@ -17,4 +19,3 @@ scp "$1" $remotehost:stl/
 ssh $remotehost "python /opt/skeinforge/skeinforge_application/skeinforge_utilities/skeinforge_craft.py stl/$(basename $filename)" | strings
 
 scp $remotehost:stl/$(basename "${filename%.*}_export.gcode") $outname
-
